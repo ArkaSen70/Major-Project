37f34c179be3a06c2b29adc4e85ee7a209068bf3
Arka Sen <senrick200@gmail.com>
2022-06-04 23:41:43 +0530

    first commit

---
+++ client/src/components/Discuss.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Comments from "./comments/Comments";
+
+const Discuss = () => {
+  return (
+    <div>
+      <Comments
+        commentsUrl="http://localhost:3004/comments"
+        currentUserId="1"
+      />
+    </div>
+  );
+};
+
+export default Discuss;
